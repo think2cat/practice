ceac7848fc810ac6b720b1b47a05d0129591ffdf
郭润桂 <gavin2026@gmail.com>
2018-04-16 17:30:15 +0800

    完成心跳出版，存在bug

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -5,19 +5,26 @@ var WebSocket = require('ws');
 var wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-    console.log('server: receive connection.');
-    
-    ws.on('message', function incoming(message) {
-        console.log('server: received: %s', message);
-    });
+  console.log('server: receive connection.');
+  let i = 0;
+  ws.on('message', (message) => {
+    try{
+      console.log('server: received: %s', message);
+      ws.send("hello" + i++);
+    }catch(e){
+      console.log("ws server is error", e);
+    }
+  });
 
-    setInterval(() => {
-		ws.send('world: ' + new Date());
-	},5000);
+  //setInterval(() => {
+  //ws.send('world: ' + new Date());
+  //},5000);
 });
-
+console.log("websocket server is listening...");
+/*
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
 app.listen(3000);
+*/
